aa10b0e9cc1e09eeeb3f3ff34935aee0d01f4929
young <youngblue-zero@daum.net>
2017-10-09 15:48:41 +0900

    터치 이벤트 추가.

--- drawCtrl.js
+++ drawCtrl.js
@@ -1,23 +1,31 @@
+var canvas = document.getElementById("myCanvas"); 
+var ctx = canvas.getContext("2d");
 
-    var canvas = document.getElementById("myCanvas"); 
-    var ctx = canvas.getContext("2d");
-    
-    var x = canvas.width / 2;
-    var y = canvas.height / 2;
-    var dx = -2;
-    var dy = 2;
-    
-    var clearView = function(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    
-    var drawCircle = function(){
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI*2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-        x += dx;
-        y += dy;
-    }
-    
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+var canvasRect = canvas.getBoundingClientRect();
+var x = canvasWidth / 2;
+var y = canvasHeight / 2;
+var dx = -2;
+var dy = 2;
+
+var getTouchPos = function(touchEvent) {
+  return {
+    x: touchEvent.touches[0].clientX - canvasRect.left,
+    y: touchEvent.touches[0].clientY - canvasRect.top
+  };
+}
+
+var clearView = function(){
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+var drawCircle = function(){
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
